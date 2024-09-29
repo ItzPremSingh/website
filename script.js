2edf0c782440bc475d431898522a58e25b8fa0c4
Prem Singh <142191827+ItzPremSingh@users.noreply.github.com>
2024-09-29 16:15:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,32 @@ async function fetchJsonObject(filePath) {
         if (!response.ok) {
             throw new Error('Network response was not ok: ' + response.statusText);
         }
-        const jsonData = await response.json(); 
+        const jsonData = await response.; 
         return jsonData; 
     } catch (error) {
         return null; 
     }
 }
 
-(async function() {
-  const dict = await fetchJsonObject("pages.json");
+// (async function() {
+//   const dict = await fetchJsonObject("https://itzpremsingh.github.io/website/pages.json");
+
+//     console.log(dict);
   
-  for (const [name, { repository, description }] of Object.entries(dict)) {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-          <h3>${name}</h3>
-          <p>${description}</p>
-          <a href="https://itzpremsingh.github.io/${repository}/" target="_blank">View Project</a>
-      `;
-    webpageList.appendChild(card);
-  }
+//   for (const [name, { repository, description }] of Object.entries(dict)) {
+//     const card = document.createElement("div");
+//     card.className = "card";
+//     card.innerHTML = `
+//           <h3>${name}</h3>
+//           <p>${description}</p>
+//           <a href="/${repository}/" target="_blank">View Project</a>
+//       `;
+//     webpageList.appendChild(card);
+//   }
+// })();
+
+(async function() {
+const dict = await fetchJsonObject("https://itzpremsingh.github.io/website/pages.json");
+console.log("Fetched data:", dict);
 })();
+ //         <a href="https://itzpremsingh.github.io/${repository}/" target="_blank">View Project</a>
