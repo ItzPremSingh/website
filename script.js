872da6c03ccec41abb336f56496810f652a67c48
Prem Singh <142191827+ItzPremSingh@users.noreply.github.com>
2024-09-29 15:26:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 "use strict";
 
-const dict = {
-  Table: {
-    url: "https://itzpremsingh.github.io/table/",
-    description: "Write table for the given number.",
-  },
-  Mines: {
-    url: "https://itzpremsingh.github.io/mines/",
-    description: "Game of mines and bomb.",
-  },
-};
+const fs = require('fs');
+
+const jsonData = fs.readFileSync('pages.json', 'utf8');
+const jsonObject = JSON.parse(jsonData);
 
 const webpageList = document.getElementById("webpage-list");
 
-for (const [name, { url, description }] of Object.entries(dict)) {
+for (const [name, { repository, description }] of Object.entries(dict)) {
   const card = document.createElement("div");
   card.className = "card";
   card.innerHTML = `
         <h3>${name}</h3>
         <p>${description}</p>
-        <a href="${url}" target="_blank">View Project</a>
+        <a href="https://itzpremsingh.github.io/${repository}/" target="_blank">View Project</a>
     `;
   webpageList.appendChild(card);
 }
