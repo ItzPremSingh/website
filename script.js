0a5cce286eda37f288f98ac4be9356816e6e1d0d
Prem Singh <142191827+ItzPremSingh@users.noreply.github.com>
2024-09-29 16:51:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,31 @@
 const webpageList = document.getElementById("webpage-list");
 
 async function fetchJsonObject(filePath) {
-    try {
-        const response = await fetch(filePath);
-        if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
-        }
-        const jsonData = await response.text(); 
-        return jsonData; 
-    } catch (error) {
-        return null; 
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error("Network response was not ok: " + response.statusText);
     }
+    const jsonData = await response.text();
+    return JSON.parse(jsonData);
+  } catch (error) {
+    return {};
+  }
 }
 
-// (async function() {
-//   const dict = await fetchJsonObject("https://itzpremsingh.github.io/website/pages.json");
+(async function () {
+  const dict = await fetchJsonObject(
+    "https://itzpremsingh.github.io/website/pages.json"
+  );
 
-//     console.log(dict);
-  
-//   for (const [name, { repository, description }] of Object.entries(dict)) {
-//     const card = document.createElement("div");
-//     card.className = "card";
-//     card.innerHTML = `
-//           <h3>${name}</h3>
-//           <p>${description}</p>
-//           <a href="/${repository}/" target="_blank">View Project</a>
-//       `;
-//     webpageList.appendChild(card);
-//   }
-// })();
-
-(async function() {
-const dict = await fetchJsonObject("https://itzpremsingh.github.io/website/pages.json");
-console.log("Fetched data:", dict);
+  for (const key in dict) {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+          <h3>${key}</h3>
+          <p>${dict[key]["description"]}</p>
+          <a href="https://itzpremsingh.github.io/${dict[key]["repository"]}/" target="_blank">View Project</a>
+      `;
+    webpageList.appendChild(card);
+  }
 })();
- //         <a href="https://itzpremsingh.github.io/${repository}/" target="_blank">View Project</a>
